8f523ddd0f1e8cac4bfcd3ad1b374535d04930aa
lecielpourlamer <88413718+lecielpourlamer@users.noreply.github.com>
2024-12-18 09:10:34 +0900

    2nd commit

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,15 +1,12 @@
 package guru.springframework;
 
-public class Dollar {
-
-    public int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        //amount = amount * multiplier;
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
